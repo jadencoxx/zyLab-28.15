1319e0c57df7aba42f82ef0f8f76f4742a26298e
Jaden Cox <152207393+jadencoxx@users.noreply.github.com>
2023-12-05 12:02:12 -0600

    Update main.cpp
    print menu fuction

--- main.cpp
+++ main.cpp
@@ -4,8 +4,14 @@
 using namespace std;
 
 void PrintMenu(const string playlistTitle) {
-   /* Type your code here */
-   
+   cout << endl << playlistTitle << " PLAYLIST MENU" << endl;
+   cout << "a - Add song" << endl;
+   cout << "d - Remove song" << endl;
+   cout << "c - Change position of song" << endl;
+   cout << "s - Output songs by specific artist" << endl;
+   cout << "t - Output total time of playlist (in seconds)" << endl;
+   cout << "o - Output full playlist" << endl;
+   cout << "q - Quit" << endl;
 }
 
 PlaylistNode* ExecuteMenu(char option, string playlistTitle, PlaylistNode* headNode) {
